ebabd75444542dd9bc2cd57382e3287f4dc04c85
jessica kilby <jessicakilby@gmail.com>
2016-12-04 10:06:16 -0600

    auth.html using materialize forms

--- javascripts/BookConstants.js
+++ javascripts/BookConstants.js
@@ -1,9 +1,9 @@
 "use strict";
 
 bookcase.constant("FIREBASE_CONFIG", {
-    apiKey: "AIzaSyA184uw_bjvv-4XzuR5B4tkw5WpSgJEK7g",
-    authDomain: "bookcase-it.firebaseapp.com",
-    databaseURL: "https://bookcase-it.firebaseio.com",
-    storageBucket: "bookcase-it.appspot.com",
-    messagingSenderId: "1011961589978"
+    apiKey: "AIzaSyDCQquBxiMODz7RuhBAkmVyxhM5hflHE_4",
+    authDomain: "bookcase-it-8fe9c.firebaseapp.com",
+    databaseURL: "https://bookcase-it-8fe9c.firebaseio.com",
+    storageBucket: "bookcase-it-8fe9c.appspot.com",
+    messagingSenderId: "322805739898"
   });
