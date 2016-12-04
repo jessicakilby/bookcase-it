da25e46d251cbd03b9d52ade640063a48d9441e5
jessica kilby <jessicakilby@gmail.com>
2016-12-04 11:05:04 -0600

    post and get users UserFactory.js

--- javascripts/factories/UserFactory.js
+++ javascripts/factories/UserFactory.js
@@ -1,3 +1,38 @@
 "use strict";
 
-console.log("hey");
+bookcase.factory("UserFactory", function($q, $http, FIREBASE_CONFIG){
+
+	let postUserToFB = function(authorizationData){
+		return $q((resolve, reject)=>{
+			$http.post(`${FIREBASE_CONFIG.databaseURL}/users.json`, JSON.stringify({
+				uid: authorizationData.uid,
+				username: authorizationData.username
+			}))
+			.success(function(responseFromPost){
+				resolve(responseFromPost);
+			})
+			.error(function(fromPostError){
+				reject(fromPostError);
+			});
+		});
+	};
+
+	let getUserFromFB = function(userId){
+		return $q((resolve, reject)=>{
+			$http.get(`${FIREBASE_CONFIG.databaseURL}users.json?orderBy="uid"$equalTo="${userId}"`)
+			.success(function(responseFromGet){
+				let users = [];
+				Object.keys(responseFromGet).forEach(function(key){
+					users.push(responseFromGet[key]);
+				});
+				resolve(users[0]);
+			}).error(function(errorFromGet){
+				reject(errorFromGet);
+			});
+		});
+	};
+	return {
+		postUserToFB:postUserToFB,
+		getUserFromFB:getUserFromFB
+	};
+});
