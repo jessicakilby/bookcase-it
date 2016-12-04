54beab22ab40e3591def3c46886ca567ae6fce9f
jessica kilby <jessicakilby@gmail.com>
2016-12-04 08:09:01 -0600

    made controllers and factories files, adding to BookConfig.js

--- javascripts/BookConfig.js
+++ javascripts/BookConfig.js
@@ -8,3 +8,31 @@ let isAuth = (AuthFactory)=>new Promise((resolve, reject)=>{
 	}
 });
 
+bookcase.run(function($rootScope, $location, FIREBASE_CONFIG, AuthFactory){
+	firebase.initializeApp(FIREBASE_CONFIG);
+	$rootScope.$on("$routeChangeStart", function(event, currRoute, preRoute){
+		let logged = AuthFactory.isAuthenticated();
+		let appTo;
+		if(currRoute.originalPath){
+			appTo = currRoute.originalPath.indexOf("/auth") !== -1;
+		}
+		if(!appTo && !logged) {
+			event.preventDefault();
+			$location.path("/auth");
+		}
+	});
+});
+
+bookcase.config(function($routeProvider){
+	$routeProvider
+		.when("/auth", {
+			templateUrl: "partials/auth.html",
+			controller: "AuthCtrl"
+		})
+		.when("/bookcase/home", {
+			templateUrl: "partials/bookcase-home.html",
+			controller: "ContactListCtrl",
+			resolve: {isAuth}
+		})
+		.otherwise("/auth");
+});
