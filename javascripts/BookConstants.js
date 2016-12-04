ca4600d2959980972c74fe5575170e88fcb784d9
jessica kilby <jessicakilby@gmail.com>
2016-12-04 10:33:56 -0600

    new firebase project, deleted old file with keys, new keys in file and added to .gitignore

--- javascripts/BookConstants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-bookcase.constant("FIREBASE_CONFIG", {
-    apiKey: "AIzaSyDCQquBxiMODz7RuhBAkmVyxhM5hflHE_4",
-    authDomain: "bookcase-it-8fe9c.firebaseapp.com",
-    databaseURL: "https://bookcase-it-8fe9c.firebaseio.com",
-    storageBucket: "bookcase-it-8fe9c.appspot.com",
-    messagingSenderId: "322805739898"
-  });
