98d4df3fd1514e70a9c9b5164dc464e1603cca34
jessica kilby <jessicakilby@gmail.com>
2016-12-04 21:15:29 -0600

    can add new books and they add to firebase

--- javascripts/controllers/AddBookCtrl.js
+++ javascripts/controllers/AddBookCtrl.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log("hey");
+bookcase.controller("AddBookCtrl", function($scope, $rootScope, $location, BookFactory){
+
+	$scope.newBook = {};
+
+	$scope.submitNewBook = function(){
+		$scope.newBook.loanedOut = false;
+		$scope.newBook.uid = $rootScope.user.uid;
+		BookFactory.postBookFB($scope.newBook)
+		.then(function(bookId){
+			$location.url("/bookcase/home");
+			$scope.newBook = {};
+		});
+	};
+});	
