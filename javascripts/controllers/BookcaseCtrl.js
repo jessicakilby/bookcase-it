4e48261e8bce56e90764d911759500d0dec488b9
jessica kilby <jessicakilby@gmail.com>
2016-12-06 12:23:39 -0600

    said no styling but I had to follow that rabbit!

--- javascripts/controllers/BookcaseCtrl.js
+++ javascripts/controllers/BookcaseCtrl.js
@@ -21,10 +21,4 @@ bookcase.controller("BookcaseCtrl", function($scope, $rootScope, BookFactory){
 		});
 	};
 
-	$scope.loanedOutChbx = function(checkbox){
-		console.log("checkbox clicked");
-		BookFactory.editBookFB(checkbox)
-		.then(function(response){});
-	};
-
 });
